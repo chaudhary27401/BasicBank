6048f68fe396319846bcf3216de7681fb1e97da8
chaudhary27401 <yc27401@gmail.com>
2025-10-28 14:31:18 +0000

    New Changes

--- db/sqlc/accounts_test.go
+++ db/sqlc/accounts_test.go
@@ -3,17 +3,17 @@ package db
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/chaudhary27401/BasicBank/db/util"
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateAccount(t *testing.T) {
-
+func CreateRandomAccount(t *testing.T) Account {
 	args := CreateAccountParams{
-		Owner: RandomOwner(),
-
-		Balance:  2.10,
-		Currency: "Rupee",
+		Owner:    util.RandomOwner(),
+		Balance:  util.RandomMoney(),
+		Currency: util.RandomCurrency(),
 	}
 	account, err := testQueries.CreateAccount(context.Background(), args)
 	require.NoError(t, err)
@@ -25,6 +25,63 @@ func TestCreateAccount(t *testing.T) {
 	require.NotZero(t, account.CreatedAt)
 }
 
-// func TestGetAccount(t *testing.T){
-// 	args:= Get
-// }
+func TestCreateAccount(t *testing.T) {
+	CreateRandomAccount(t)
+}
+
+func TestGetAccount(t *testing.T) {
+	account1 := CreateRandomAccount(t)
+	account2, err := testQueries.GetAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.Equal(t, account1.ID, account2.ID)
+	// require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+	require.WithinDuration(t, account1.CreatedAt.Time, account2.CreatedAt.Time, time.Second)
+}
+func TestUpdateAccount(t *testing.T) {
+	account1 := CreateRandomAccount(t)
+	arg := UpdateAccountParams{
+		ID:      account1.ID,
+		Balance: account1.Balance,
+	}
+	account2, err := testQueries.UpdateAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, account2)
+	require.Equal(t, account1.Owner, account2.Owner)
+	require.Equal(t, account1.Balance, account2.Balance)
+	require.Equal(t, account1.Currency, account2.Currency)
+	require.Equal(t, account1.ID, account2.ID)
+	// require.WithinDuration(t, account1.CreatedAt, account2.CreatedAt, time.Second)
+	require.WithinDuration(t, account1.CreatedAt.Time, account2.CreatedAt.Time, time.Second)
+}
+
+func TestDeleteAccount(t *testing.T) {
+	account1 := CreateRandomAccount(t)
+	err := testQueries.DeleteAccount(context.Background(), account1.ID)
+	require.NoError(t, err)
+	// account2, err = testQueries.DeleteAccount(context.Background(), account1.ID)
+	// require.Error(t, err)
+	// require.EqualError(t, err, sql.ErrNoRows.Error())
+	// require.Empty(account2)
+
+}
+
+func TestListAccount(t *testing.T) {
+	for i := 0; i < 10; i++ {
+		CreateRandomAccount(t)
+	}
+	arg := ListAccountParams{
+		Limit:  5,
+		Offset: 5,
+	}
+	account, err := testQueries.ListAccount(context.Background(), arg)
+	require.NoError(t, err)
+	require.Len(t, account, 5)
+	for _, acc := range account {
+		require.NotEmpty(t, acc)
+	}
+
+}
